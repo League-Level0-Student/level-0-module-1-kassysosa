48733ca1de1df763420943b215e75aa47c1ae2e5
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-31 13:26:06 -0700

    SecretMessage,Riddler

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -14,18 +14,36 @@ import javax.swing.JOptionPane;
  */
 
 public class SecretMessageBox {
-
 	// 0. Make a main method and put steps 1-5 inside it
 	
-	// 1. Set a password in a String variable
+	public static void main(String[] args) {
+		
 	
+	// 1. Set a password in a String variable
+	String thisisThePassword = "xo";
+
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
+	
+String SecretMessage = JOptionPane.showInputDialog("What is the secret message?");
 
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
+JOptionPane.showMessageDialog(null, "You can only see the secret message if you guess the password");
+String guess = JOptionPane.showInputDialog("Enter password");
 
+if(guess.equals(thisisThePassword)) {
+	//show secret message 
+	JOptionPane.showMessageDialog(null, SecretMessage);
+}
 	// 4. If their guess matches the password, show them the secret message
 
-	// 5. If the password does not match, pop-up "INTRUDER!!"
 
+
+	// 5. If the password does not match, pop-up "INTRUDER!!"
+else {
+// do not show secret message 
+JOptionPane.showMessageDialog(null, "Intruder!");
+
+}
+}
 }
--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,11 +10,14 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String answer = JOptionPane.showInputDialog(null, "What belongs to you, but is used by everyone else");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+String riddle = "your name";
+if(answer.equals(riddle))
+	//pop up 
+	JOptionPane.showMessageDialog(null, "Correct!");
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
